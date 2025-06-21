bedbf152b253362b7867bc7b17e7351e68cfddd8
hfarahani <hfarahani7@gmail.com>
2025-06-21 15:31:29 -0400

    Fix footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,13 +5,11 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
-        <p>&copy; {new Date().getFullYear()} JEANICEHUANGMUA LLC</p>
-        <p>All Rights Reserved</p>
-        <p style={{ fontSize: '0.9rem', marginTop: '0.5rem' }}>
-          Built by Henry Farahani 2025
-        </p>
-        <p>
-          <a href="https://henryfarahani.com">henryfarahani.com</a>
+        <p style={{ fontSize: '0.9rem' }}>
+          &copy; {new Date().getFullYear()} JEANICEHUANGMUA LLC <br />
+          All Rights Reserved <br />
+          Built by Henry Farahani 2025 <br />
+          <a href="https://henryfarahani.com">henryfarahani.com</a> <br />
         </p>
       </div>
     </footer>
